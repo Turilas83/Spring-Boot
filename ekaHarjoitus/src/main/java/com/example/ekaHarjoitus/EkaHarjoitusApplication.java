eb220a55224b66cce880c9e93895f3ed2097f885
Mikko Kaasinen <kaasinenmikko@gmail.com>
2018-01-24 00:30:30 +0200

    Add files via upload

---
+++ ekaHarjoitus/src/main/java/com/example/ekaHarjoitus/EkaHarjoitusApplication.java
@@ -0,0 +1,12 @@
+package com.example.ekaHarjoitus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkaHarjoitusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkaHarjoitusApplication.class, args);
+	}
+}
---
+++ ekaHarjoitus/src/main/java/com/example/ekaHarjoitus/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.example.ekaHarjoitus.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class DemoController {
+	
+	@RequestMapping("/index")
+	public String Index() {
+		return "This is the main page";
+	}
+	@RequestMapping("/contact")
+	public String contact() {
+		return "This is the contact page";
+	}
+
+}
