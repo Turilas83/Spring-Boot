ae6849ba8fa2edd55047f0a878acf898d7f2dd77
Mikko Kaasinen <kaasinenmikko@gmail.com>
2018-01-30 22:57:06 +0200

    Add files via upload

---
+++ helloThymeleaf/src/main/java/com/example/helloThymeleaf/controller/MyController.java
@@ -0,0 +1,18 @@
+package com.example.helloThymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MyController {
+	
+	@RequestMapping("/hello")
+	public String greeting(@RequestParam(value = "name") String name, @RequestParam(value = "age") int age, Model model) {
+		model.addAttribute("name", name);
+		model.addAttribute("age", age);
+		return "index";
+	}
+
+}
